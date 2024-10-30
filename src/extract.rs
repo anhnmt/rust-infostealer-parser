d353ed7a5c052268f6156f749d325ae16967fd4c
Tuan Anh <anhng.dev@gmail.com>
2024-10-30 11:19:05 +0700

    Refactor extract to struct for more flexibility

--- src/extract.rs
+++ src/extract.rs
@@ -6,48 +6,82 @@ pub const ZIP: &str = ".zip";
 pub const RAR: &str = ".rar";
 pub const SEVEN_Z: &str = ".7z";
 
-pub fn extract_file(base: &str, file: &str) -> Result<Vec<String>, Box<dyn Error>> {
-    let mut archive = Archive::new(file).open_for_processing()?;
-    let mut files = Vec::new();
+#[derive(Debug, Default)]
+pub struct Extract {
+    pub base: &'static str,
+    pub file: &'static str,
+    pub whitelists: HashSet<&'static str>,
+}
 
-    let files_to_extract = HashSet::from([
-        "UserInformation.txt",
-        "Passwords.txt",
-        "All Passwords.txt",
-    ]);
-
-    while let Some(header) = archive.read_header()? {
-        let entry = header.entry();
-
-        archive = if entry.is_file() {
-            match entry.filename.file_name().and_then(|os_str| os_str.to_str()) {
-                Some(file_name) if files_to_extract.contains(&file_name.trim()) => {
-                    files.push(entry.filename.to_string_lossy().to_string());
-                    header.extract_with_base(base)?
-                }
-                _ => header.skip()?,
-            }
-        } else {
-            header.skip()?
-        };
-
+impl Extract {
+    pub fn new(file: &'static str) -> Self {
+        Self {
+            file,
+            ..Default::default()
+        }
     }
 
-    // println!("{:?} files extracted", files);
-    Ok(files)
+    pub fn with_base(mut self, base: &'static str) -> Self {
+        self.base = base;
+        self
+    }
+
+    pub fn with_whitelists(mut self, whitelists: HashSet<&'static str>) -> Self {
+        self.whitelists = whitelists;
+        self
+    }
+
+    pub fn extract_file(self) -> Result<Vec<String>, Box<dyn Error>> {
+        let mut archive = Archive::new(self.file).open_for_processing()?;
+        let mut files: Vec<String> = Vec::new();
+
+        while let Some(header) = archive.read_header()? {
+            let entry = header.entry();
+
+            archive = if entry.is_file() {
+                match entry.filename.file_name().and_then(|os_str| os_str.to_str()) {
+                    Some(file_name) if self.whitelists.is_empty() || self.whitelists.contains(&file_name.trim()) => {
+                        files.push(entry.filename.to_string_lossy().to_string());
+                        header.extract_with_base(self.base)?
+                    }
+                    _ => header.skip()?,
+                }
+            } else {
+                header.skip()?
+            };
+        }
+
+        // println!("{:?} files extracted", files);
+        Ok(files)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::extract::extract_file;
+    use crate::extract::Extract;
+    use std::collections::HashSet;
 
     #[test]
-    fn test() {
-        let files = extract_file(
-            "test_data/extract/",
-            "test_data/@MANTICORECLOUD - 24.10 - 4300 PCS.rar",
-        );
+    fn default() {
+        let extract = Extract::new("test_data/@MANTICORECLOUD - 24.10 - 4300 PCS.rar")
+            .with_base("test_data/extract/")
+            .extract_file()
+            .unwrap();
+        assert_eq!(extract.len(), 1167);
+    }
 
-        assert_eq!(files.unwrap().len(), 159);
+    #[test]
+    fn whitelists() {
+        let extract = Extract::new("test_data/@MANTICORECLOUD - 24.10 - 4300 PCS.rar")
+            .with_base("test_data/extract/")
+            .with_whitelists(HashSet::from([
+                "UserInformation.txt",
+                "Passwords.txt",
+                "All Passwords.txt",
+            ]))
+            .extract_file()
+            .unwrap();
+
+        assert_eq!(extract.len(), 159);
     }
 }
